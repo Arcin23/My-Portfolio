d93a1258a03c275937ecc059959e3ad9eb68cfea
Arcin23 <151884149+Arcin23@users.noreply.github.com>
2024-04-22 09:21:01 +0200

    Update Projects.jsx

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { getImageUrl } from '../../util';
+import  styles  from './Projects.module.css';
 
 export const Projects = () => {
   return (
-    <section>
-        <div>
-            <h2>PROJECTS</h2>
-            <div>
+    <section  className={styles.container} id="projects">
+        <h2 className={styles.title} >PROJECTS</h2>
+        <div className={styles.cards}>
+            <div className={styles.card}>
                 <img src={(getImageUrl('project.png'))}/>
-                <h3>Weather-App</h3>
-                <p>This is web application made to tell the weather of different cities around the world</p>
+                <h3>My Portfolio</h3>
+                <p>It isresponsive web application for my personal portfolio</p>
+                <div className={styles.links}>
+                    <a href='/'><button>Demo</button></a>
+                    <a href='https://github.com/Arcin23/My-Portfolio'><button>Source</button></a>
+                </div>
             </div>
+            <div className={styles.card}>
+                <img src={(getImageUrl('project.png'))}/>
+                <h3>Weather App</h3>
+                <p>Web application to tell the weather of different cities around the world</p>
+                <div className={styles.links}>
+                    <a href='https://github.com/Arcin23/Weather-app'><button>Demo</button></a>
+                    <a href='https://github.com/Arcin23/Weather-app'><button>Source</button></a>
+                </div>
+            </div>
+                
         </div>
     </section>
   )
